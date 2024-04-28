6ab4abe5ceb4ff1d9ddefaf3adedb66109851138
Stefan Furne <stefan@furne.net>
2024-04-28 10:03:28 +0200

    Omit NA to fix exclude_missing

--- R/ivo_tables.R
+++ R/ivo_tables.R
@@ -33,7 +33,9 @@ ivo_excl_missing <- function(df, exclude_missing = FALSE, missing_string = "(Mis
         df[is.na(df[[i]]),i] <- "6049122418972891471204127890512XY"
         df[[i]] <- factor(df[[i]], levels = c(levs, "6049122418972891471204127890512XY"), labels = c(levs, missing_string))
       } else if(sum(is.na(df[[i]]))>0) { df[is.na(df[[i]]), i] <- missing_string }
-    }}
+    }} else {
+      df <- na.omit(df)
+    }
   return(df)
 }
 
