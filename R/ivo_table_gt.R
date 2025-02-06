8c6a3573137295714afd124632b7b3fe00b90d4b
Stefan Furne <stefan@furne.net>
2025-02-06 18:25:02 +0100

    Restructuring and fixing build

--- R/ivo_table_gt.R
+++ R/ivo_table_gt.R
@@ -13,11 +13,8 @@
 #' @details The functions \code{ivo_table_gt()} takes a \code{data.frame} with 1-3 columns. The order of the columns in the \code{data.frame} will determine where they will be displayed in the table. The first column will always be displayed at the top of the table. If there are more than one column the following 2-3 columns will be displayed to the left in order. To change how the columns are displayed in the table; change the place of the columns in the \code{data.frame} using \code{dplyr::select()}.
 #' @author Stefan Furne
 #' @encoding UTF-8
-#' @importFrom dplyr group_by summarize left_join mutate across
-#' @importFrom tidyr pivot_wider
 #' @importFrom gt gt tab_header tab_spanner tab_source_note tab_stubhead
-#' @examples
-#' TODO
+#' TODO @ examples
 #' @export
 ivo_table_gt <- function(df,
                          title = NULL,
@@ -27,39 +24,18 @@ ivo_table_gt <- function(df,
                          mask = FALSE,
                          missing_string = "(Missing)",
                          totals = NULL) {
-    # Capture selected columns
+    # Capture the columns present
     columns <- names(df)
     num_vars <- length(columns)
 
     if (num_vars > 3) stop("The GT table function supports a maximum of three variables (3-way table).")
 
-    # Group, count, and ensure character columns
+    # Prepare the data
     df <- df |>
-        group_by(across(all_of(columns))) |>
-        summarize(Antal = n(), .groups = "drop") |>
-        replace_na(list(Antal = 0))
-
-    # Pivot for 2-way or 3-way tables
-    if (num_vars %in% c(2, 3)) {
-        # Get unique values for all selected columns so we get a complete table
-        all_combinations <- expand.grid(lapply(df[as.character(columns)], unique))
-
-        # Join back to original df and pivot
-        df <- all_combinations |>
-            left_join(df, by = as.character(columns)) |>
-            pivot_wider(names_from = first(columns), values_from = "Antal", values_fill = 0)
-    }
-
-    # Ensure we use the missing_string across all string values and fill zeroes for numerical
-    df <- df |>
-        mutate(
-            across(where(~ is.character(.) | is.factor(.)), ~ replace_na(as.character(.), missing_string)),
-            across(where(is.numeric), ~ replace_na(.x, 0))
-        )
+        group_and_count(columns) |>
+        missing_string_and_zeros(missing_string)
 
     # Create the GT table object
-    gt_table <- df |> gt()
-
     # If it's a 2-way or 3-way table we'll set the row name and group name
     if (num_vars %in% c(2, 3)) {
         gt_table <- df |> gt(
@@ -69,18 +45,17 @@ ivo_table_gt <- function(df,
         if (num_vars == 2) {
             gt_table <- gt_table |> tab_stubhead(columns[2])
         }
-        # Get the groups right in the table
+    } else {
+        gt_table <- gt(df)
     }
 
     # Apply optionals
-    if (is.character(title)) {
-        gt_table <- gt_table |>
-            tab_header(title = title)
-    }
-
-    if (is.character(title) & is.character(subtitle)) {
-        gt_table <- gt_table |>
-            tab_header(title = title, subtitle = subtitle)
+    if (!is.null(title)) {
+        if (!is.null(subtitle)) {
+            gt_table <- gt_table |> tab_header(title = title, subtitle = subtitle)
+        } else {
+            gt_table <- gt_table |> tab_header(title = title)
+        }
     }
 
     if (extra_header) {
@@ -94,4 +69,50 @@ ivo_table_gt <- function(df,
     }
 
     return(gt_table)
+}
+
+#' @title Grouped and summarized \code{data.frame}
+#' @description Creates a data.frame that is grouped and summarized with frequencies to be used with GT
+#' @param df A \code{data.frame} object.
+#' @param columns A character vector of column names.
+#' @return A \code{data.frame} with grouped and summarized counts.
+#' @noRd
+#' @importFrom dplyr group_by summarize left_join across all_of first n distinct select
+#' @importFrom tidyr pivot_wider crossing
+group_and_count <- function(df, columns) {
+    # Group and count columns
+    df <- df |>
+        group_by(across(all_of(columns))) |>
+        summarize(n = n(), .groups = "drop")
+
+    # Handle 2-way or 3-way tables
+    if (length(columns) %in% c(2, 3)) {
+        # Get combinations for all selected variables so we get a complete table
+        all_combinations <- df |>
+            select(all_of(columns)) |>
+            distinct() |>
+            crossing()
+
+        # Join back to original df and pivot
+        df <- all_combinations |>
+            left_join(df, by = columns) |>
+            pivot_wider(names_from = first(columns), values_from = "n")
+    }
+
+    return(df)
+}
+
+#' @title Replace missing values in a \code{data.frame}
+#' @description Replaces missing values in a `data.frame` by assigning a custom string to character or factor columns, and replacing missing numeric values with zeros.
+#' @param df A \code{data.frame} object.
+#' @param missing_string A string to replace missing values in character or factor columns.
+#' @return A \code{data.frame} with missing values replaced according to the specified rules.
+#' @noRd
+#' @importFrom dplyr mutate across where
+#' @importFrom tidyr replace_na
+missing_string_and_zeros <- function(df, missing_string) {
+    df |> mutate(
+        across(where(~ is.character(.) | is.factor(.)), ~ replace_na(as.character(.), missing_string)),
+        across(where(is.numeric), ~ replace_na(.x, 0))
+    )
 }
